233a039e4325db9f2da9166ef0750636b7ea8216
Wilson Wei Xiang Chee <wichee@c1r3s1.42singapore.sg>
2024-10-05 12:31:13 +0800

    added minishell.h as prereq for lexer.h and future modules

---
+++ includes/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wichee <wichee@student.42singapore.sg      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/05 12:07:31 by wichee            #+#    #+#             */
+/*   Updated: 2024/10/05 12:16:59 by wichee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd,h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
