6b3b90d8a4fe2d8f0836ee3ffe0f89866d5608b7
Wilson Wei Xiang Chee <wichee@c1r3s1.42singapore.sg>
2024-11-20 15:16:19 +0800

    complete merge

--- minishell.c
+++ minishell.c
@@ -97,8 +97,8 @@ int	main(int ac, char **av, char **envp)
 		}
 		free(input);
 		ft_free(&token_data, &ast_root);
-		execution(exec_data, &env, input);
-		execution_with_pipes(exec_data, &env, input);
+		execution(exec_data, &env);
+		execution_with_pipes(exec_data, &env);
 		execution(exec_data, &env);
 		execution_with_pipes(exec_data, &env);
 		ft_lstclear(&exec_data, ft_free_exec_data);
