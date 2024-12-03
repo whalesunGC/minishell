94a275212b5414f5ec965b2161c6ba060053ed93
wichee <wichee@42mail.sutd.edu.sg>
2024-12-03 22:08:17 +0800

    issue 78

--- 05_Execution/multiple_commands_utils8.c
+++ 05_Execution/multiple_commands_utils8.c
@@ -67,7 +67,7 @@ void	increment_counters_and_traverse_next_node(
 int	handle_forking_process_and_executing_child(
 			t_piping_multiple_command_params *params, char ***env, int i)
 {
-	if (*params->exit_status != 0)
+	if (*params->exit_status != 0 && *params->exit_status != 1)
 		return (-1);
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
